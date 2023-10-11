c40b2f48b13433bf484b244faf7cfb7b32c9a996
ElMaikina <mikesoto890@gmail.com>
2023-10-11 19:47:01 -0300

    solved fourth exercise

--- MinSizeSum.java
+++ MinSizeSum.java
@@ -1,58 +1,36 @@
-//Given an array of n positive integers and a positive integer s, find the minimal length
-//of a subarray of which the sum ≥ s. If there isn’t one, return 0 instead.
-//For example, given the array [2,3,1,2,4,3] and s = 7, the subarray [4,3] has the minimal
-//length of 2 under the problem constraint
+// Given an array of n positive integers and a positive integer s, find the minimal length
+// of a subarray of which the sum ≥ s. If there isn’t one, return 0 instead.
+// For example, given the array [2,3,1,2,4,3] and s = 7, the subarray [4,3] has the minimal
+// length of 2 under the problem constraint
 import java.io.*; 
 import java.lang.*; 
 import java.util.*;
 
-public class TwoSum {
+public class MinSizeSum {
     public static void main(String[] args) {
         // Input variables
-        int[] numbers = {1, 3, 1, 4, 99, 64, 12, 12, 2, 7, 11, 15};
-        int target = 24;
-
-        System.out.println("Arreglo de números: " + Arrays.toString(numbers));
-        System.out.println("Número objetivo: " + target);
-
-        // Output variables
-        int index1 = 0;
-        int index2 = 0;
+        List<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(4);
+        numbers.add(3);
+        int targetSum = 7;
         int sum = 0;
 
-        // First solution using two loops
-        System.out.println("\nResolución (usando dos ciclos)\n");
-                
-        for (; index1 < numbers.length; index1++) {
-            for (index2 = index1 + 1; index2 < numbers.length; index2++) {
-                sum = numbers[index1] + numbers[index2];
-                System.out.println("    Primer número: " + numbers[index1] + ", Segundo número: " + numbers[index2] + ", Suma = " + sum);
-                if (sum == target) {
-                    System.out.println("\nEl número objectivo se alcanza con los indices " + index1 + " y " + index2);
-                    index1 = numbers.length;
-                    index2 = numbers.length;
-                    break;
-                }
-            }
-        }
+        // My solution consist of reading the array from the biggest
+        // element to the smallest. This way, it is faster to find the
+        // smallest subarray since the Sum grows as fast as it can.
+        System.out.println("Numeros (No Ordenados): " + Arrays.toString(numbers.toArray()));
+        Collections.sort(numbers);
+        System.out.println("Numeros (Ordenados): " + Arrays.toString(numbers.toArray()));
 
-        // Second solution using HashMap
-        System.out.println("\nResolución (usando Hashmap)\n");
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        int[] result = new int[2];
-                
-        for (index1 = 0; index1 < numbers.length; index1++) {
-            System.out.print("Número leído: " + numbers[index1] + ", Índice: " + index1 + " ");
-            if (map.containsKey(numbers[index1])) {
-                int index = map.get(numbers[index1]);
-                result[0] = index;
-                result[1] = index1;
-                System.out.println("\n\nEl número objectivo se alcanza con los indices " + result[0] + " y " + result[1]);
-                break;
-            }
-            else {
-                map.put(target - numbers[index1], index1);
-                System.out.println("(Se agregó al HashMap: " + (target - numbers[index1]) + ", " + index1 + ")");
+        for (int i = numbers.size() - 1; i > 0 ; i--) {
+            sum += numbers.get(i);
+            if (sum >= targetSum) {
+                System.out.println("Largo minimo: " + (numbers.size() - i));
+                return;
             }
         }
     }
