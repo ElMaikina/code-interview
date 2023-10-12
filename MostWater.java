a5a029e0ff9f0bd61a908f9201d20af45c9044bf
ElMaikina <mikesoto890@gmail.com>
2023-10-11 21:57:28 -0300

    started new exercise

---
+++ MostWater.java
@@ -0,0 +1,13 @@
+//Given an array and a value, remove all instances of that value in place and return the
+//new length. (Note: The order of elements can be changed. It doesn’t matter what you
+//leave beyond the new length.)
+import java.io.*; 
+import java.lang.*; 
+import java.util.*;
+
+public class MostWater {
+    public static void main(String[] args) {
+        // Input variables
+        int[] numbers = {1, 1, 2, 3, 10, 00, 4, 4, 5, 6, 7, 7, 7, 7, 7, 99, 22, 1, 5, 0, 6};
+    }
+}
